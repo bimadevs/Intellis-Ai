8a8777cfd3d7fd01052fad4fb5bb9fc47106175d
bimajov12 <bimajov12@gmail.com>
2024-09-10 09:39:53 +0000

    update navbar

--- src/app/components/ui/navbar.tsx
+++ src/app/components/ui/navbar.tsx
@@ -16,7 +16,7 @@ function NavbarMenu({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 text-white text-sm md:text-lg", className)}
     >
       <Menu setActive={setActive}>
         <a href="/">Home</a>
