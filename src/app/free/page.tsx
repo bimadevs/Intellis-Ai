6422d2b7c3e8a17d14e778fe89371d8a6be1da51
bimajov12 <bimajov12@gmail.com>
2024-09-11 02:37:43 +0000

    update link code ai

--- src/app/free/page.tsx
+++ src/app/free/page.tsx
@@ -1,5 +1,5 @@
 import Card from "./ui/card";
-import { InputLoader } from "../components/ui/input-loader";
+import { InputLoader } from "./ui/input-loader";
 import { Hero } from "./ui/hero";
 import { Code } from "./ui/code";
 import { TextHover } from "./ui/text-hover";
--- src/app/free/ui/input-loader.tsx
+++ src/app/free/ui/input-loader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import { MultiStepLoader as Loader } from "../acertenity/multi-step-loader";
+import { MultiStepLoader as Loader } from "../../components/acertenity/multi-step-loader";
 import { IconSquareRoundedX } from "@tabler/icons-react";
 
 const loadingStates = [
@@ -47,7 +47,7 @@ export function InputLoader() {
                     <h1 className="text-lg md:text-xl font-bold ">ChatBot</h1>
                     <div className="flex justify-center items-center w-full gap-8">
                         <button
-                            onClick={() => handleButtonClick('<iframe src="https://app.vectorshift.ai/chatbots/embedded/66dee915292e6e0ef6a2b07b?openChatbot=true" width="500px" height="600px" style="border: none; position: fixed; bottom: 0; right: 0; margin: 10px;" allow="clipboard-read; clipboard-write"/>')}
+                            onClick={() => handleButtonClick('<iframe src="https://shorturl.at/l9yQM" width="500px" height="600px" style="border: none; position: fixed; bottom: 0; right: 0; margin: 10px;" allow="clipboard-read; clipboard-write"/>')}
                             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                             style={{
                                 boxShadow: "0px -1px 0px 0px #ffffff40 inset, 0px 1px 0px 0px #ffffff40 inset",
@@ -57,7 +57,7 @@ export function InputLoader() {
                         </button>
 
                         <button
-                            onClick={() => handleButtonClick('<script async id="vectorshift-chat-widget" src="https://app.vectorshift.ai/chatWidget.js" chatbot-id="66dee915292e6e0ef6a2b07b" chatbot-height="600px" chatbot-width="400px" />')}
+                            onClick={() => handleButtonClick('<script async id="vectorshift-chat-widget" src="https://shorturl.at/4KIRR" chatbot-id="66dee915292e6e0ef6a2b07b" chatbot-height="600px" chatbot-width="400px" />')}
                             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                             style={{
                                 boxShadow: "0px -1px 0px 0px #ffffff40 inset, 0px 1px 0px 0px #ffffff40 inset",
@@ -69,7 +69,7 @@ export function InputLoader() {
                     <h1 className="text-lg md:text-xl font-bold ">Image generator</h1>
                     <div className="flex justify-center items-center w-full gap-8">
                         <button
-                            onClick={() => handleButtonClick('<iframe src="https://app.vectorshift.ai/forms/embedded/66d6fa869d99492d174837eb" width="1000px" height="900px" allow="clipboard-read; clipboard-write microphone"/>')}
+                            onClick={() => handleButtonClick('<iframe src="https://shorturl.at/wfAez" width="1000px" height="900px" allow="clipboard-read; clipboard-write microphone"/>')}
                             className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
                             style={{
                                 boxShadow: "0px -1px 0px 0px #ffffff40 inset, 0px 1px 0px 0px #ffffff40 inset",
