a1a8c7633a94bc75b3fe82a88c6c8d20b1529a66
bimajov12 <bimajov12@gmail.com>
2024-09-11 01:15:36 +0000

    update free page

--- src/app/free/ui/code.tsx
+++ src/app/free/ui/code.tsx
@@ -4,7 +4,7 @@ import { Compare } from "@/app/components/acertenity/compare";
 export function Code() {
   return (
     <div className="flex justify-center flex-col items-center p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
-     <h1 className="mb-4 text-xl">Full View and Embed example code </h1>
+     <h1 className="mb-4 text-xl">Full View and Pop up example code </h1>
       <Compare
         firstImage="/images/FullView.webp"
         secondImage="/images/embed.webp"
