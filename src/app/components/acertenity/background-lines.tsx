3ab7df4403333461148495f7915b7bb39f90a7da
bimajov12 <bimajov12@gmail.com>
2024-09-12 04:14:49 +0000

    update hero free (fixed)

--- src/app/components/acertenity/background-lines.tsx
+++ src/app/components/acertenity/background-lines.tsx
@@ -17,7 +17,7 @@ export const BackgroundLines = ({
   return (
     <div
       className={cn(
-        "h-[20rem] md:h-screen w-full bg-white dark:bg-black",
+        "h-screen md:h-screen w-full bg-white dark:bg-black",
         className
       )}
     >
